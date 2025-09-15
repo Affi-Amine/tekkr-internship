494f84c534d75bb35b88acf72facbf3337773056
Moritz Bruder <moritz@mbruder.com>
2025-09-15 15:54:52 +0200

    add onclick handler

--- web/src/components/chat-sidebar.tsx
+++ web/src/components/chat-sidebar.tsx
@@ -14,9 +14,9 @@ interface Props {
 }
 
 export function ChatSidebar(props: Props) {
-  const { chats } = props;
+  const { chats, selectedChatId } = props;
   return <div className={"flex flex-col border-r-accent border-r-2 h-full w-64 fixed left-0 top-16 bottom-0 p-4 gap-3"}>
-    <Button size={"sm"}>
+    <Button onClick={props.onCreateChat} size={"sm"}>
       <PlusIcon className={"w-5 h-5"}/>
       New Chat
     </Button>
@@ -25,7 +25,7 @@ export function ChatSidebar(props: Props) {
       { chats.map((chat => (
           <div key={chat.id}>
             <Button
-              variant={props.selectedChatId === chat.id ? "secondary" : "ghost"}
+              variant={selectedChatId === chat.id ? "secondary" : "ghost"}
               size={"sm"}
               className={"w-full text-left justify-start"}
               onClick={() => props.onSelectChat?.(chat.id)}
